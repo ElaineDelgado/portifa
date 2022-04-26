cd59e6f44ba0e501e005068daaedd7e2a9504873
Elaine Delgado <delgado.elaine03@gmail.com>
2022-04-26 15:50:41 -0300

    media queries reponsive

--- src/pages/Info.tsx
+++ src/pages/Info.tsx
@@ -13,9 +13,7 @@ const Info = () => {
           <div className="perfil-text">
             <p>
               Olá! Meu nome é Elaine Delgado, sou desenvolvedora frontend e moro no Rio de Janeiro. 
-              Comecei a estudar as tecnologias necessárias para desenvolvimento de software em 2019, mas o que conquistou meu coração foi o Frontend.
-            </p>
-            <p>
+              Comecei a estudar as tecnologias necessárias para desenvolvimento de software em 2019, mas o que conquistou meu coração foi o Frontend.            
               No início de 2020 comecei um trabalho na área de Frontend mas mesmo não paro de estudar novas tecnologias e aquelas já utilizo.        
             </p>          
           </div>
