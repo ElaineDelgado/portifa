ace29d7f83b52ed03d2b4002ffe32a5bfa5f552d
Elaine Delgado <delgado.elaine03@gmail.com>
2022-06-10 16:39:07 -0300

    corrigindo commit anterior

---
+++ src/data/work.ts
@@ -0,0 +1,38 @@
+import superarj from '../assets/images/superarj.png'
+import pactorj from '../assets/images/pactorj.png'
+
+export const works = [
+  {
+    work: 'SuperaRJ',
+    image: superarj,
+    url: "https://superarj.rj.gov.br/",
+  },
+  {
+    work: 'PactoRJ',
+    image: pactorj,
+    url:"http://www.pacto.rj.gov.br/",
+  },
+]
+
+export const projects = [  
+  {
+    work: '1-2 anos de experiência',
+    image: 'xxxx',
+    url:"Javascript",
+  },
+  {
+    work: '1-2 anos de experiência',
+    image: 'xxxx',
+    url: "Bootstrap",
+  },
+  {
+    work: '1-2 anos de experiência',
+    image: 'xxxx',
+    url: "SASS",
+  },
+  {
+    work: '1-2 anos de experiência',
+    image: 'xxxx',
+    url: "GITHUB",
+  },
+]
