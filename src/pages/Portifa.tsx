97048b873689d077515a2792eec025c70e5f22c8
Elaine Delgado <delgado.elaine03@gmail.com>
2022-06-11 16:46:39 -0300

    adicionando Tailwindcss ao projeto

--- src/pages/Portifa.tsx
+++ src/pages/Portifa.tsx
@@ -6,23 +6,21 @@ import Subtitle from '../components/Subtitle'
 const Portifa = () => {
   return (
     <section className="portifa-section" id="portifa">
-      <Title title='Portifolio'/> 
-      <Subtitle title={'em construção'} />     
+      <Title title='Portifolio'/>    
   
-      <Subtitle title={'principais projetos profissionais e pessoais'} />
-
-      <article className="works-cards-container">
-        {works.map((item, index) => (
-          <div key={index} className="work-card">
-            <h2>{item.work}</h2>
-            <section className="linkToProject">
-              <p>Link:</p> 
-              <a href={item.url} target="_blank" rel="noreferrer">Clique aqui</a>
-            </section>
-            <img src={item.image} alt={item.work} />
-          </div>
-        ))}
-      </article>
+      <Subtitle title={'principais projetos profissionais e pessoais'} />     
+        <article className="works-cards-container grid grid-cols-1 md:grid-cols-2 pt-8">
+          {works.map((item, index) => (
+            <div key={index} className="work-card">
+              <h2>{item.work}</h2>
+              <section className="linkToProject">
+                <p>Link:</p> 
+                <a href={item.url} target="_blank" rel="noreferrer">Clique aqui</a>
+              </section>
+              <img src={item.image} alt={item.work} />
+            </div>
+          ))}
+        </article>      
     </section>
   )
 }
