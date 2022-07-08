96a3107ed58df5d840bf6b144d70142439faf011
Elaine Delgado <delgado.elaine03@gmail.com>
2022-07-08 15:23:50 -0300

    incluindo projeto weather app

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,17 +2,18 @@ import superarj from '../assets/images/superarj.png'
 import pactorj from '../assets/images/pactorj.png'
 import album from '../assets/images/album.png'
 import quiz from '../assets/images/quiz.png'
+import weatherapp from '../assets/images/weatherapp.png'
 
 export const works = [
   {
     title: 'SuperaRJ',
-    description:'Trabalho nesse projeto desde sua criação em 2021, e tenho orgulho de poder contribuir com esse trabalho até hoje. Iniciei fazendo as telas da aplicação, e hoje além de contribuir com código, também participo das decisões para melhorias e novas implementações.',
+    description:'Trabalho nesse projeto desde sua criação em 2021. Iniciei fazendo as telas da aplicação, e hoje além de contribuir com código, também participo das decisões para melhorias e novas implementações.',
     image: superarj,
     url: "https://superarj.rj.gov.br/",
   },
   {
     title: 'PactoRJ',
-    description:'Mais detalhes em breve.',
+    description:'Em meu trabalho também contribuo no desenvolvimento e manutenção do Portal Transparência do PactoRJ, em que é possível conferir o andamento de cada um dos projetos que integram o pacote de obras.',
     image: pactorj,
     url:"http://www.pacto.rj.gov.br/",
   },
@@ -21,14 +22,20 @@ export const works = [
 export const personal = [  
   {
     title: 'Photo Album JsonPlaceholder',
-    description:'Mais detalhes em breve.',
+    description:'Protótipo de ábum de fotos construído com React e Typescript utilizando a API JsonPlaceholder.',
     image: album,
     url:"https://albums-app.netlify.app",
   },
   {
     title: 'Anime Quiz',
-    description:'Mais detalhes em breve.',
+    description:'Quiz desenvolvido como parte do treianamento CJRM, objetivando a manipulação do DOM através do Javascript.',
     image: quiz,
+    url: "https://quiz-cjrm.netlify.app",
+  },
+  {
+    title: 'Current Weather',
+    description:'Projeto desenvolvido durante treinamento de Javascript CJRM. O projeto utiliza Javascript e a API utilizada para as requisições foi a Weather API.',
+    image: weatherapp,
     url: "https://quiz-cjrm.netlify.app",
   },
 ]
